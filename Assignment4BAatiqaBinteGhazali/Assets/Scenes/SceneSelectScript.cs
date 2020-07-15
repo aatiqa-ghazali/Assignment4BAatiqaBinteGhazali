51f08159a92e8a62c9e19c488ba5eba9699c328c
Aatiqa Bint e Ghazali <>
2020-07-15 02:27:19 -0700

    Done
    Done

---
+++ Assignment4BAatiqaBinteGhazali/Assets/Scenes/SceneSelectScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSelectScript : MonoBehaviour
+{
+    public void GotoPenguin()
+    {
+        SceneManager.LoadScene("Penguins");
+    }
+    public void GotoHummingbird()
+    {
+        SceneManager.LoadScene("FlowerIsland");
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+    public void BacktoMenu()
+    {
+        SceneManager.LoadScene("MainMenu"); ;
+    }
+}
